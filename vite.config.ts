795f5cd7a788e43292fb397cc801f9ca786bbadf
Vu Tong <tonghoangvu@outlook.com>
2024-07-27 13:13:33 +0700

    Config environment variables for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import { ValidateEnv, Schema } from "@julr/vite-plugin-validate-env";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    ValidateEnv({
+      VITE_BUILD_NUMBER: Schema.string.optional(),
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
